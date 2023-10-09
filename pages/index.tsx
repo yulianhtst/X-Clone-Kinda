e5928ce48fbec23d457333716c7dc2ee630f4293
Yulian <yulianst7@gmail.com>
2023-10-09 19:12:18 +0300

    Add landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import LoginComponent from '@/components/Login';
 import Head from 'next/head'
 
 export default function Home() {
@@ -10,9 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>
-        Hello
-      </h1>
+      <LoginComponent />
     </>
   )
 }
