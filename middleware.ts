637d03a00059ace39fc36b14c425e0e427f4761c
Yulian <yulianst7@gmail.com>
2024-06-19 16:18:38 +0300

    -Test

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 
+console.log(1);
 
 
 export async function middleware(req: NextRequest) {
