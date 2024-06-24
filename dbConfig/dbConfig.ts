c2a59048ba002b357a3380583711ddfa13260c28
Yulian <yulianst7@gmail.com>
2024-06-24 16:28:14 +0300

    bugfix(server)/Add connection string to dbConfig

--- dbConfig/dbConfig.ts
+++ dbConfig/dbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDb() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect("mongodb://localhost:27017/");
     console.log("db connected");
   } catch (error) {
     console.error(error);
